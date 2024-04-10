ce3431311d4d6ec3a12b377ce1a328d1dd15496b
Ankit Amonkar <ankit.amonkar@gmail.com>
2024-04-10 02:45:39 -0400

    Fixing youtubeSearch tests

--- townService/src/api/youtubeSearch.test.ts
+++ townService/src/api/youtubeSearch.test.ts
@@ -1,47 +1,47 @@
 // Only works when API is working. Had to comment it out because test was failing since API is down
-// import express from 'express';
-// import request from 'supertest';
-// import fetchMock from 'jest-fetch-mock';
-// import router from './youtubeSearch';
+import express from 'express';
+import request from 'supertest';
+import fetchMock from 'jest-fetch-mock';
+import router from './youtubeSearch';
 
-// beforeAll(() => {
-//   fetchMock.enableMocks();
-// });
+beforeAll(() => {
+  fetchMock.enableMocks();
+});
 
-// beforeEach(() => {
-//   fetchMock.resetMocks();
-// });
+beforeEach(() => {
+  fetchMock.resetMocks();
+});
 
-// const app = express();
-// app.use(express.json());
-// app.use('/', router);
+const app = express();
+app.use(express.json());
+app.use('/', router);
 
-// describe('Testing for YoutubeSearch', () => {
-//   it('should require a query parameter', async () => {
-//     const response = await request(app).get('/youtube-search');
-//     expect(response.status).toBe(400);
-//     expect(response.text).toBe('Query parameter is required.');
-//   });
+describe('Testing for YoutubeSearch', () => {
+  //   it('should require a query parameter', async () => {
+  //     const response = await request(app).get('/youtube-search');
+  //     expect(response.status).toBe(400);
+  //     expect(response.text).toBe('Query parameter is required.');
+  //   });
 
-//   it('should handle YouTube API errors gracefully', async () => {
-//     fetchMock.mockReject(new Error('YouTube API returned an error.'));
-//     const response = await request(app).get('/youtube-search?query=test');
-//     expect(response.status).toBe(500);
-//     expect(response.text).toBe('Internal Server Error');
-//   });
+  it('should handle YouTube API errors gracefully', async () => {
+    fetchMock.mockReject(new Error('YouTube API returned an error.'));
+    const response = await request(app).get('/youtube-search?query=test');
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Internal Server Error');
+  });
 
-//   it('should return YouTube API data successfully', async () => {
-//     process.env.REACT_APP_YOUTUBE_API_KEY = 'AIzaSyBQx2knfxTyJxo7IyQsbSwkFCzBvRiYGWE';
+  //   it('should return YouTube API data successfully', async () => {
+  //     process.env.REACT_APP_YOUTUBE_API_KEY = 'AIzaSyBQx2knfxTyJxo7IyQsbSwkFCzBvRiYGWE';
 
-//     const response = await request(app).get('/youtube-search?query=test');
+  //     const response = await request(app).get('/youtube-search?query=test');
 
-//     expect(response.status).toBe(200);
+  //     expect(response.status).toBe(200);
 
-//     expect(response.body).toHaveProperty('items');
-//     expect(response.body.items).toBeInstanceOf(Array);
+  //     expect(response.body).toHaveProperty('items');
+  //     expect(response.body.items).toBeInstanceOf(Array);
 
-//     expect(response.body.items[0]).toHaveProperty('id');
-//     expect(response.body.items[0].id).toHaveProperty('videoId');
-//     expect(response.body.items[0].snippet).toHaveProperty('title');
-//   });
-// });
+  //     expect(response.body.items[0]).toHaveProperty('id');
+  //     expect(response.body.items[0].id).toHaveProperty('videoId');
+  //     expect(response.body.items[0].snippet).toHaveProperty('title');
+  //   });
+});
